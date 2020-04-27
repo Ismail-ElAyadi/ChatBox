134c2c0767d0b4709a7e8df99e52227993214fca
Rifi Ismail <ismail.elydi@gmail.com>
2020-04-27 08:33:52 +0200

    state pseudo created in Connexion compenent

--- src/compenent/Connexion.js
+++ src/compenent/Connexion.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 
 export default class Connexion extends Component {
-    render() {
 
+    state = {
+        pseudo: ''
+    }
+
+    handleChange=(e)=>{
+        const pseudo = e.target.value
+        this.setState({
+            pseudo:pseudo
+        })
+    }
+    render() {
         return (
             <div className="connexionBox">
                 <form className="connexion">
                     <input
+                    value={this.state.pseudo}
+                    onChange={this.handleChange}
                     placeholder="Pseudo"
                     type="text"
                     required
