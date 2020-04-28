39f2fc412a72f4266989e61150cc64fec9a4b81f
Rifi Ismail <ismail.elydi@gmail.com>
2020-04-28 07:07:02 +0200

    Create Message , Send in the State of the app when submit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import './App.css'
 import Form from './compenent/Form'
 import Message from './compenent/Message'
 class App extends Component {
+  state = {
+    messages:{},
+    pseudo:this.props.match.params.pseudo
+  }
+  addMessage = msg => {
+    const messages = {...this.state.messages}
+    messages[`message-${Date.now()}`] = msg
+    this.setState({
+      messages
+    })
+  }
   render () {
     return (
       <div className='box' >
@@ -11,7 +22,10 @@ class App extends Component {
             <Message/>
           </div>
         </div>
-        <Form/>
+        <Form
+        addMessage={this.addMessage}
+        pseudo={this.state.pseudo}
+        />
       </div>
     )
   }
--- src/compenent/Form.js
+++ src/compenent/Form.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 
 export default class Form extends Component {
+    state = {
+        message:''
+
+    }
+    createMessage = ()=> {
+        const {addMessage,pseudo} = this.props
+        const message = {
+            pseudo,
+            message:this.state.message
+        }
+        addMessage(message)
+    }
+
+    handleSubmit=(e)=> {
+        e.preventDefault();
+        this.createMessage()
+
+    }
+    handleChange=(e)=> {
+        this.setState({
+            message : e.target.value
+        })
+    }
     render() {
         return (
-            <form className='form'>
+            <form className='form' onSubmit={this.handleSubmit}>
                 <textarea
+                onChange={this.handleChange}
                 required
                 maxLength='140'/>
                 <div className='info'>
